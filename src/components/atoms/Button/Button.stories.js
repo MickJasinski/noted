7b02d11b44afd69356583be8522252632a9ec411
Michal Jasinski <michal.jasinski@xigen.co.uk>
2020-02-28 13:24:38 +0000

    Updare

--- src/components/atoms/Button/Button.stories.js
+++ src/components/atoms/Button/Button.stories.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, select } from '@storybook/addon-knobs';
-import Button from 'components/atoms/Button';
+import Button from './Button';
 
 storiesOf('Button', module)
   .addDecorator(withKnobs)
   .add('Primary', () => {
     const label = 'Colors';
     const options = {
-      Red: 'red',
-      Blue: 'blue',
-      Yellow: 'yellow',
+      Primary: 'hsl(49, 100%, 58%)',
+      Secondary: 'hsl(196, 83%, 75%)',
+      Tertiary: 'hsl(106, 47%, 64%)',
     };
-    const defaultValue = 'red';
+    const defaultValue = 'hsl(49, 100%, 58%)';
     const groupId = 'GROUP-ID1';
-
     const value = select(label, options, defaultValue, groupId);
-    return <Button color={value}>Click this</Button>;
+    return <Button color={value}>Hello Roman</Button>;
   })
-  .add('Secondary', () => <Button secondary>then this</Button>);
+  .add('Secondary', () => <Button secondary>Hello Roman</Button>);
